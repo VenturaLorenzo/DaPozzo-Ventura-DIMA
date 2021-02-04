b96aea20650acffb774f2f15d7c85c9e0ec4e0db
AndreaDaPozzo <andreamauro.dapozzo@mail.polimi.it>
2021-02-04 19:22:45 +0100

    Sistemo errore di merge

--- Development/dapozzo_ventura_app/lib/ui/user_profile_icon.dart
+++ Development/dapozzo_ventura_app/lib/ui/user_profile_icon.dart
@@ -57,53 +57,49 @@ class UserProfileIcon extends StatelessWidget {
         ),
       );
     } else {
-      return Container(
-        height: height,
-        child: Padding(
-          padding: EdgeInsets.all(height / 10),
-          child: Column(
-            children: [
-              SizedBox(
-                  height: height / 2,
-                  width: height / 2,
-                  child: Padding(
-                      padding: EdgeInsets.all(height / 20),
-                      child: ClipOval(
-                        child: Image.network(
-                          "https://media.gqitalia.it/photos/5ec3ca47a63ee8cb452d9ce4/master/pass/Avatar.jpg",
-                          fit: BoxFit.cover,
-                        ),
-                      ))),
-              Container(
-                height: height / 10,
-                child: Text(
-                  "Mario Rossi",
-                  style: TextStyle(
-                    fontSize: height / 17,
-                    color: Colors.blue[300],
-                  ),
+      return Padding(
+        padding: const EdgeInsets.fromLTRB(5, 30, 5, 5),
+        child: Column(
+          children: [
+            SizedBox(
+                height: 125,
+                width: 125,
+                child: Padding(
+                    padding: EdgeInsets.all(5),
+                    child: ClipOval(
+                      child: Image.network(
+                        "https://media.gqitalia.it/photos/5ec3ca47a63ee8cb452d9ce4/master/pass/Avatar.jpg",
+                        fit: BoxFit.cover,
+                      ),
+                    ))),
+            Padding(
+              padding: const EdgeInsets.all(1),
+              child: Text(
+                "Mario Rossi",
+                style: TextStyle(
+                  color: Colors.blue[300],
                 ),
               ),
-              Container(
-                height: height / 10,
-                child: Text(
-                  "mario.rossi@gmail.com",
-                  style:
-                      TextStyle(fontSize: height / 17, color: Colors.blue[200]),
-                ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(1),
+              child: Text(
+                "mario.rossi@gmail.com",
+                style: TextStyle(color: Colors.blue[200]),
               ),
-              Container(
-                height: height / 10,
-                child: Text(
-                  "LOGOUT",
-                  style: TextStyle(
-                      fontSize: height / 17,
-                      color: Colors.blue[200],
-                      decoration: TextDecoration.underline),
-                ),
+            ),
+            TextButton(
+              child: Text(
+                "LOGIN",
+                style: TextStyle(
+                    color: Colors.blue[400],
+                    decoration: TextDecoration.underline),
               ),
-            ],
-          ),
+              onPressed: () {
+                Navigator.pushNamed(context, "/login");
+              },
+            ),
+          ],
         ),
       );
     }
